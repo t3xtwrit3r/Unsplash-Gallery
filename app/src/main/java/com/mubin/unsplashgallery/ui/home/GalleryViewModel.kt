3064e8a96be6e13c4746605d1d0d0af6f867f006
t3xtwrit3r <maksudmubin@gmail.com>
2022-07-24 02:42:14 +0600

    feat: Save image file to gallery using Download Manager

--- app/src/main/java/com/mubin/unsplashgallery/ui/home/GalleryViewModel.kt
+++ app/src/main/java/com/mubin/unsplashgallery/ui/home/GalleryViewModel.kt
@@ -1,22 +1,29 @@
 package com.mubin.unsplashgallery.ui.home
 
+import android.app.Application
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.haroldadmin.cnradapter.NetworkResponse
 import com.mubin.unsplashgallery.api.models.responseModel.UnsplashPhotoItem
 import com.mubin.unsplashgallery.repository.AppRepository
+import com.mubin.unsplashgallery.ui.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
 class GalleryViewModel
 @Inject
-constructor(private val repository: AppRepository) : ViewModel() {
+constructor(private val repository: AppRepository, application: Application) : BaseViewModel(application) {
 
     private val _dataLoading = MutableLiveData<Boolean>()
     val dataLoading: LiveData<Boolean> = _dataLoading
@@ -52,6 +59,33 @@ constructor(private val repository: AppRepository) : ViewModel() {
                     }
                 }
             }
+        }
+    }
+
+    fun onSaveClicked(authorName: String, photoLink: String) {
+
+        downloadImageNew(authorName+"${System.currentTimeMillis()}", photoLink)
+
+    }
+
+    private fun downloadImageNew(filename: String, downloadUrlOfImage: String) {
+        try {
+            val dm = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager?
+            val downloadUri: Uri = Uri.parse(downloadUrlOfImage)
+            val request = DownloadManager.Request(downloadUri)
+            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+                .setAllowedOverRoaming(false)
+                .setTitle(filename)
+                .setMimeType("image/jpeg") // Your file type. You can use this code to download other file types also.
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                .setDestinationInExternalPublicDir(
+                    Environment.DIRECTORY_PICTURES,
+                    File.separator.toString() + filename + ".jpg"
+                )
+            dm!!.enqueue(request)
+            Toast.makeText(context, "Image download started.", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Toast.makeText(context.applicationContext, "Image download failed.", Toast.LENGTH_SHORT).show()
         }
     }
 
--- app/src/main/java/com/mubin/unsplashgallery/ui/viewPhoto/ViewPhotoFragment.kt
+++ app/src/main/java/com/mubin/unsplashgallery/ui/viewPhoto/ViewPhotoFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.mubin.unsplashgallery.api.models.bindingModel.PhotoLinkAuthor
 import com.mubin.unsplashgallery.databinding.FragmentViewPhotoBinding
 import com.mubin.unsplashgallery.ui.HomeActivity
+import com.mubin.unsplashgallery.ui.home.GalleryViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ViewPhotoFragment : Fragment() {
 
     private lateinit var binding: FragmentViewPhotoBinding
+
+    private val viewPhotoViewModel: GalleryViewModel by viewModels()
 
     private var photoLink = ""
     private var authorName = ""
@@ -21,7 +28,10 @@ class ViewPhotoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentViewPhotoBinding.inflate(layoutInflater, container, false)
+        binding = FragmentViewPhotoBinding.inflate(layoutInflater, container, false).apply{
+            viewModel = viewPhotoViewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
         return binding.root
     }
 
