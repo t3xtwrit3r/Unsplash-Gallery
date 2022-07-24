a6090908ed24963b594d6718e4de8a0686bb8d72
t3xtwrit3r <maksudmubin@gmail.com>
2022-07-25 02:24:52 +0600

    feat: HomeCommunicator interface methods implemented in HomeActivity to ask for Storage permission fix: Toolbar title updates using HomeCommunicator interface's implemented method in HomeActivity

--- app/src/main/java/com/mubin/unsplashgallery/ui/HomeActivity.kt
+++ app/src/main/java/com/mubin/unsplashgallery/ui/HomeActivity.kt
@@ -1,17 +1,23 @@
 package com.mubin.unsplashgallery.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.mubin.unsplashgallery.R
 import com.mubin.unsplashgallery.databinding.ActivityMainBinding
+import com.mubin.unsplashgallery.helper.Session
+import com.mubin.unsplashgallery.ui.home.HomeCommunicator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), HomeCommunicator {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -22,6 +28,7 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        isCheckPermission()
         initNavGraph()
         initToolbar()
 
@@ -36,10 +43,56 @@ class HomeActivity : AppCompatActivity() {
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 
-    fun setToolbarTitle(title: String){
+    override fun setToolbarTitle(title: String){
 
         binding.toolbar.title = title
 
+    }
+
+    override fun askPermission() {
+
+        isCheckPermission()
+
+    }
+
+    private fun isCheckPermission(): Boolean {
+        val permission1 = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        val permission2 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        return when {
+            permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED -> {
+                true
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) || shouldShowRequestPermissionRationale(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) -> {
+                requestMultiplePermissions.launch(
+                    arrayOf(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                )
+                false
+            }
+            else -> {
+                requestMultiplePermissions.launch(
+                    arrayOf(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                )
+                false
+            }
+        }
+    }
+
+    private val requestMultiplePermissions = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+        permissions.entries.forEach { permission ->
+            if (permission.key == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                Session.isRead = permission.value
+            }
+            if (permission.key == Manifest.permission.WRITE_EXTERNAL_STORAGE) {
+                Session.isWrite = permission.value
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/mubin/unsplashgallery/ui/home/HomeCommunicator.kt
@@ -0,0 +1,6 @@
+package com.mubin.unsplashgallery.ui.home
+
+interface HomeCommunicator {
+    fun setToolbarTitle(title: String)
+    fun askPermission()
+}
