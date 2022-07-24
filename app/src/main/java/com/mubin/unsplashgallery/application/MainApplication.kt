ff69e51bfd8d759c2c0008c0f7a72ebbf444a8db
t3xtwrit3r <maksudmubin@gmail.com>
2022-07-25 02:21:42 +0600

    feat: Session object created and Initialized in MainApplication class

--- app/src/main/java/com/mubin/unsplashgallery/application/MainApplication.kt
+++ app/src/main/java/com/mubin/unsplashgallery/application/MainApplication.kt
@@ -1,7 +1,16 @@
 package com.mubin.unsplashgallery.application
 
 import android.app.Application
+import com.mubin.unsplashgallery.helper.Session
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication: Application ()
+class MainApplication: Application (){
+
+
+    override fun onCreate() {
+        super.onCreate()
+        Session.init(this)
+    }
+
+}
---
+++ app/src/main/java/com/mubin/unsplashgallery/helper/Session.kt
@@ -0,0 +1,47 @@
+package com.mubin.unsplashgallery.helper
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.annotation.NonNull
+import androidx.core.content.edit
+
+object Session {
+
+    private val Pref_Name = "com.mubin.unsplashgallery.app.session"
+
+    private lateinit var pref: SharedPreferences
+
+    private val Is_Write = "isWrite"
+    private val Is_Read = "isRead"
+
+    fun init(@NonNull context: Context) {
+        pref = context.getSharedPreferences(Pref_Name, Context.MODE_PRIVATE)
+    }
+
+    fun clearSession() {
+        pref.edit {
+            clear()
+        }
+    }
+
+    var isWrite: Boolean
+        get() {
+            return pref.getBoolean(Is_Write, false)
+        }
+        set(value) {
+            pref.edit {
+                putBoolean(Is_Write, value)
+            }
+        }
+
+    var isRead: Boolean
+        get() {
+            return pref.getBoolean(Is_Read, false)
+        }
+        set(value) {
+            pref.edit {
+                putBoolean(Is_Read, value)
+            }
+        }
+
+}
